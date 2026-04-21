09b810fdf065bb3e61b1a76cd0a67c9f2feedb67
Samuel Gonzalez <samuelgonzalez@sclogin2.rc.usf.edu>
2026-04-21 00:38:12 -0400

    added txt files containing simple i/o

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
-// were going to do some stuff with trees here. 
-// just testing student cluster connectivity here
-//
+// project requirements:
+// 	read tree data from files
+// 	contruct tree dynamically
+// 	analyze tree properties
+// 	allow interactive exploration
+
+
+// 	use a linked structure implementation
+//	will be given two files: tree-car.txt & tree-investment.txtc
